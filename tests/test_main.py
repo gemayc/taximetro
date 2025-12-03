90316d82580f75f303457dc786af8c121d15b914
gemayc <gemita284@hotmail.com>
2025-12-03 13:27:49 +0100

    test: add unit tests for fare calculation using unittest #12

---
+++ tests/test_main.py
@@ -0,0 +1,24 @@
+import unittest
+import sys
+import os
+
+# Esto permite importar main.py desde la carpeta src
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+from main import calculate_fare
+
+
+class TestCalculateFare(unittest.TestCase):
+    def test_calculate_fare_basic(self):
+        result = calculate_fare(10, 10)
+        self.assertEqual(result, 0.2 + 0.5)  # 10 * 0.02 + 10 * 0.05
+    
+    def test_calculate_fare_only_stopped(self):
+        result= calculate_fare(10, 0)
+        self.assertEqual(result, 0.2)
+    
+    def test_calculate_fare_only_moving(self):
+        result= calculate_fare(0, 10)
+        self.assertEqual(result, 0.5) 
+
+if __name__ == "__main__":
+    unittest.main()
